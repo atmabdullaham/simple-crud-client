c456271f51c7e0b029e0c540ed170cfc65db0ced
ATM Abdullah Al Mamun <atmabdullaham@gmail.com>
2025-03-11 12:40:02 +0600

    completed the crud operation

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -2,18 +2,41 @@ import { useLoaderData } from "react-router-dom";
 
 const Update = () => {
   const loadedData = useLoaderData();
-  console.log(loadedData);
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const updatedUser = { name, email };
+    console.log(updatedUser);
+
+    //
+    fetch(`http://localhost:5000/users/${loadedData._id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updatedUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          alert("user updated successfully");
+        }
+      });
+  };
   return (
     <div>
       Update:{loadedData.name}
-      <form>
+      <form onSubmit={handleUpdate}>
         <input type="text" name="name" id="" defaultValue={loadedData?.name} />
         <br />
         <input
           type="email"
           name="email"
           id=""
-          defaultValue={loadedData.email}
+          defaultValue={loadedData?.email}
         />
         <br />
         <input type="submit" value="Update" />
